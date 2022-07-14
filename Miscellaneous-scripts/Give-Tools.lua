6cf63c0b55517b5d58965b4a3db92043c5956415
Elden <69537751+RegularID@users.noreply.github.com>
2022-07-14 11:36:41 +0300

    A new method of giving tools
    To use it just call GiveToolsPlayer(Player), not sure if this could be used for other things I just found out about it today
    
    Thanks for shownape for the parent tools in other tools method; https://github.com/ShownApe/newest-releases/blob/main/fencing%20spray%20dupe.lua.
    Thanks for nixon for helping me test the script

---
+++ Miscellaneous-scripts/Give-Tools.lua
@@ -0,0 +1,67 @@
+local wait = task.wait
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local Player = Players.LocalPlayer or Players:GetPropertyChangedSignal("LocalPlayer"):Wait() and Players.LocalPlayer
+local Backpack = Player:WaitForChild("Backpack")
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local Humanoid = Character:WaitForChild("Humanoid")
+
+local Tool = Character:FindFirstChild("Punch") or Backpack:FindFirstChild("Punch")
+if not Tool then
+    Tool = Backpack.ChildAdded:Wait()
+    wait()
+end
+
+local function LoadHandles()
+    Humanoid:UnequipTools()
+    for _, v in ipairs(Backpack:GetChildren()) do
+        if v:IsA("Tool") and v ~= Tool then
+    	    v.Parent = Character
+    	    wait()
+    		  v.Parent = Backpack
+        end
+    end
+end
+
+local function SetToolsParent()
+    LoadHandles()
+    Humanoid:UnequipTools()
+    wait()
+    for _, v in ipairs(Backpack:GetChildren()) do
+        if v:IsA("Tool") and v ~= Tool then
+          -- not exactly sure what this is but we need this for replicating the parent to the tool; test it yourself using roblox studio
+    	    v.Parent = Character
+    		  v.Parent = Tool
+    		  v.Parent = Backpack
+    		  v.Parent = Tool
+        end
+        
+        wait()
+    end
+end
+
+
+local function GiveToolsPlayer(Target: Player)
+    SetToolsParent()
+    local tCharacter = typeof(Target) == "Instance" and Target:IsA("Player") and Target.Character
+    if not tCharacter then return end
+    
+    local tHumanoid = tCharacter:FindFirstChild("Humanoid")
+    local tRoot = tHumanoid and tHumanoid.RootPart
+    if not tRoot then return end
+    
+    local Tools = {}
+    for _, Tool in ipairs(Tool:GetChildren()) do
+        if not Tool:IsA("Tool") then continue end
+        table.insert(Tools, Tool)
+    end
+    Tool.Parent = Character
+    for _, Tool in ipairs(Tools) do
+        local Handle = Tool:WaitForChild("Handle")
+        firetouchinterest(Handle, tRoot, 0)
+    end
+end
+
+--GiveToolsPlayer(Players.bignamernixon) -- thanks for helping me debug and stuff
